2eb95bee29844a5db519dd2f98e15171bf6f4e40
Nordy Vlasman <nordyvla@gmail.com>
2021-04-09 10:20:44 +0200

    Initial Commit

---
+++ ElSierd/ElSierdApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElSierdApp.swift
+//  ElSierd
+//
+//  Created by Nordy Vlasman on 09/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct ElSierdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
